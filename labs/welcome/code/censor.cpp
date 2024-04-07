b6445bbdfaf98f858751c69710cf77e93f0a96cd
BryceInouye <binouye@ucsb.edu>
2024-04-06 22:13:53 -0700

    More bug fixes to censor.cpp

--- labs/welcome/code/censor.cpp
+++ labs/welcome/code/censor.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cctype>
 #include <string>
+#include <vector>
 using namespace std;
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -10,28 +11,25 @@ int main(int argc, char *argv[]) {
     string word, sentence;
     getline(cin, sentence);
     size_t wordCensor = stoi(argv[1]);
+    vector <string> words;
     for (size_t i = 0; i < sentence.length(); i++) {
         if (!isblank(sentence[i])) {
             word += sentence[i];
-        } else {
-            if (word.length() != wordCensor) {
-                cout << word;
-                bool lastWord = false;
-                for (size_t k = i; k < sentence.length(); k++) {
-                    if (!isblank(sentence[k])) {
-                        lastWord = true;
-                    }
-                    k++;
-                }
-                if (lastWord) {
-                    cout << " ";
-                }
-            }
+        } else if (isblank(sentence[i]) && word.length() != 0) {
+            words.push_back(word);
             word.clear();
         }
     }
-    if (word.length() != wordCensor) {
-        cout << word;
+    if (words.size() == 0){
+        words.push_back(word);
+    }
+    for (size_t j = 0; j < words.size(); j++) {
+        if (words[j].length() != wordCensor){
+            cout << words[j];
+            if(j != words.size() - 1){
+                cout << " ";
+            }
+        }
     }
     cout << endl;
 }
