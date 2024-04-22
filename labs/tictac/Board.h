3ffba52b42ab4a29759d5cdf0320d95c8169b6f7
BryceInouye <binouye@ucsb.edu>
2024-04-21 20:27:32 -0700

    commit 4/21

--- labs/tictac/Board.h
+++ labs/tictac/Board.h
@@ -1,9 +1,15 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+
 #include "Move.h"
+#include <bitset>
 
 // I recommended writing a Board class to manage your game state.
 // Here's some space for the class definition; member functions go in Board.cpp.
+class Board {
+    std::bitset<9>player;
+    Board();
+};
 
 #endif
