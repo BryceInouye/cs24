440e76360bf30d507ecf6502229ba2234fa21534
BryceInouye <binouye@ucsb.edu>
2024-05-01 01:48:35 -0700

    tree.cpp v3

--- labs/rotate/test.cpp
+++ labs/rotate/test.cpp
@@ -5,19 +5,33 @@ int main() {
     Tree tree;
 
     // Insert some elements
-    tree.insert("Texas");
-    // tree.insert("z");
-    // tree.insert("c");
-    // tree.insert("k");
-    // tree.insert("w");
-    // tree.insert("g");
-    
+    tree.insert("L");
+    tree.insert("D");
+    tree.insert("N");
+    tree.insert("C");
+    tree.insert("A");
+    tree.insert("E");
+    tree.insert("Q");
+    tree.insert("P");
+    tree.insert("S");
+
 
     // Print the tree
     std::cout << "Tree contents: \n";
     tree.print();
     std::cout << std::endl;
     tree.createArray();
+    std::cout << "finding A: expected 0, actual: " << tree.find("A");
+    std::cout << "\nfinding L: expected 4, actual: " << tree.find("L");
+    std::cout << "\nfinding P: expected 6, actual: " << tree.find("P") << std:: endl;
+    std::cout << "lookup 0: expected A, actual: " << tree.lookup(0) << std::endl;
+    std::cout << "lookup 3: expected E, actual: " << tree.lookup(3) << std::endl;
+    std::cout << "lookup 8: expected S, actual: " << tree.lookup(8) << std::endl;
+    
+    std::cout << "contains D, expected: true, actual: " << tree.contains("D") << std::endl;
+    std::cout << "contains Z, expected: false, actual: " << tree.contains("Z") << std::endl;
+    std::cout << "contains S, expected: true, actual: " << tree.contains("S") << std::endl;
+
 
     // // Check if the tree contains certain elements
     // std::cout << "Does the tree contain 'apple'? " << (tree.contains("apple") ? "Yes" : "No") << std::endl;
