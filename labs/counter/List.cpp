07e3622edd64016e97b006365dd9bd291c477aff
BryceInouye <binouye@ucsb.edu>
2024-05-23 16:36:52 -0700

    index

--- labs/counter/List.cpp
+++ labs/counter/List.cpp
@@ -23,38 +23,7 @@ Node* List::insert(const std::string KEY, int VALUE) { // insertion at the tail
     }
     return node;
 }
-void List::remove(Node* void List::remove(Node* nodeToDelete){ // removal of any node
-    // update the pointers adjacent to the node that is to be deleted
-    // end of list: delete and update tail to be tail->prev
-    // front of list: delete and update head to be head->next
-    // middle of list: make deleted node->prev point to deleted node->next, do same for other direction
-    
-    if (nodeToDelete == nullptr) return; // if node not found
-
-    if (nodeToDelete == head && nodeToDelete == tail) { // list is size 1
-        head = nullptr;
-        tail = nullptr;
-    } else if (nodeToDelete == head) { // head removal
-        head = nodeToDelete->next;
-        if (head != nullptr) { // check if head is not null before accessing its member
-            head->prev = nullptr;
-        }
-    } else if (nodeToDelete == tail) { // tail removal
-        tail = nodeToDelete->prev;
-        if (tail != nullptr) { // check if tail is not null before accessing its member
-            tail->next = nullptr;
-        }
-    } else { // middle removal
-        nodeToDelete->prev->next = nodeToDelete->next;
-        nodeToDelete->next->prev = nodeToDelete->prev;
-    }
-    delete nodeToDelete;
-}){ // removal of any node
-    // update the pointers adjacent to the node that is to be deleted
-    // end of list: delete and update tail to be tail->prev
-    // front of list: delete and update head to be head->next
-    // middle of list: make deleted node->prev point to deleted node->next, do same for other direction
-    
+void List::remove(Node* nodeToDelete){ // removal of any node. compilation error
     if (nodeToDelete == nullptr) return; // if node not found
 
     if (nodeToDelete == head && nodeToDelete == tail) { // list is size 1
